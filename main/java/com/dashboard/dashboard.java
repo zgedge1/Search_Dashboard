1632069b60c80385333276ddd6d18ddb8f05817a
Zach <zachgedge@gmail.com>
2024-01-15 16:06:20 -0500

    Adding font and placement of weather screen button

--- main/java/com/dashboard/dashboard.java
+++ main/java/com/dashboard/dashboard.java
@@ -4,10 +4,13 @@ package com.dashboard;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-public class Dashboard extends Application{
+public class dashboard extends Application{
 
     public static void main(String[] args) {
         launch(args);
@@ -20,17 +23,35 @@ public class Dashboard extends Application{
         
         primaryStage.setTitle("Dashboard");
 
+
+        // Fonts
+        Font fontHeadingLabel = Font.font("Arial", FontWeight.BOLD, 20);
+
+        // Heading Label
+        Label headingLabel = new Label("Select a category to search");
+        headingLabel.setFont(fontHeadingLabel);
+        headingLabel.setTranslateX(150);
+        headingLabel.setTranslateY(10);
+
+
         // Actionbutton to launch weather search screen: 
         
-        Button launchWeatherScreen = new Button("Search Weather");
-        launchWeatherScreen.setOnAction(e -> weatherFrame());
+        Button launchWeatherScreenButton = new Button("Search Weather");
+
+        launchWeatherScreenButton.setMaxWidth(280);
+        launchWeatherScreenButton.setMaxHeight(200);
+        launchWeatherScreenButton.setTranslateX(150);
+        launchWeatherScreenButton.setTranslateY(80);
+        launchWeatherScreenButton.setOnAction(e -> weatherFrame());        
 
         VBox dashboardFrame = new VBox();
-        dashboardFrame.getChildren().addAll(launchWeatherScreen);
+        dashboardFrame.getChildren().addAll(headingLabel,launchWeatherScreenButton);
+        
 
         Scene scene = new Scene(dashboardFrame, 600, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
     public void weatherFrame() {
