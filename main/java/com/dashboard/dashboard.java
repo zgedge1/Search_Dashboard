fa6eec4c3a06badcb09fef632fbb67f1a05bdb56
Zach <zachgedge@gmail.com>
2024-01-16 19:36:06 -0500

    Adding methods to connect to weatherAPI and search API

--- main/java/com/dashboard/dashboard.java
+++ main/java/com/dashboard/dashboard.java
@@ -1,6 +1,16 @@
 
 package com.dashboard;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.json.JSONObject;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,6 +21,9 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class dashboard extends Application{
+
+    private static String weatherApiKey = // Enter API Key;
+    private static String weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
     public static void main(String[] args) {
         launch(args);
@@ -41,7 +54,7 @@ public class dashboard extends Application{
         launchWeatherScreenButton.setMaxWidth(280);
         launchWeatherScreenButton.setMaxHeight(200);
         launchWeatherScreenButton.setTranslateX(150);
-        launchWeatherScreenButton.setTranslateY(80);
+        launchWeatherScreenButton.setTranslateY(60);
         launchWeatherScreenButton.setOnAction(e -> weatherFrame());        
 
         VBox dashboardFrame = new VBox();
@@ -54,6 +67,8 @@ public class dashboard extends Application{
         primaryStage.setResizable(false);
     }
 
+    // Weather Frame Method
+
     public void weatherFrame() {
         
         Stage weatherFrame = new Stage();
@@ -63,6 +78,52 @@ public class dashboard extends Application{
         Scene weatherScene = new Scene(weatherBox, 600, 600);
         weatherFrame.setScene(weatherScene);
         weatherFrame.show();
+    }
+
+    // Method to connect to the API 
+
+    public void searchWeather(String weatherUserInput) {
+
+        try {
+            
+            URI uri = new URI(buildWeatherApiUrl(weatherUserInput));
+            URL url = uri.toURL();
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                String line;
+
+                StringBuffer response = new StringBuffer();
+
+                while ((line = reader.readLine())!=null) {
+                    response.append(line);
+                    
+                }
+
+                parseWeatherData(response.toString());
+                
+            }
+
+
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String buildWeatherApiUrl(String weatherUserInput) {
+        return String.format("%s?q=%s&appid=%s&units=metric", weatherApiUrl, weatherUserInput, weatherApiKey);
+    }
+
+    private void parseWeatherData(String getWeatherData){
+
+        JSONObject json = new JSONObject(getWeatherData);
+        
+
+
     }
 
     
