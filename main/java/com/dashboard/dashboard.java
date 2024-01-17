e3ecdfa7873570d984ee3b41747196896a64f65b
Zach <zachgedge@gmail.com>
2024-01-17 07:28:51 -0500

    Began inserting weatherframe components: Searchbar, seachbutton

--- main/java/com/dashboard/dashboard.java
+++ main/java/com/dashboard/dashboard.java
@@ -9,12 +9,14 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -22,8 +24,12 @@ import javafx.stage.Stage;
 
 public class dashboard extends Application{
 
-    private static String weatherApiKey = // Enter API Key;
+    //Weather API Info
+    private static String weatherApiKey = "df9f6adc7d89e0757fdee3c72ae7b5eb";
     private static String weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+    //Weather Search Bar
+    private TextArea weatherSearchField; 
 
     public static void main(String[] args) {
         launch(args);
@@ -53,8 +59,8 @@ public class dashboard extends Application{
 
         launchWeatherScreenButton.setMaxWidth(280);
         launchWeatherScreenButton.setMaxHeight(200);
-        launchWeatherScreenButton.setTranslateX(150);
-        launchWeatherScreenButton.setTranslateY(60);
+        launchWeatherScreenButton.setTranslateX(160);
+        launchWeatherScreenButton.setTranslateY(40);
         launchWeatherScreenButton.setOnAction(e -> weatherFrame());        
 
         VBox dashboardFrame = new VBox();
@@ -71,16 +77,45 @@ public class dashboard extends Application{
 
     public void weatherFrame() {
         
+        // Weather Frame
         Stage weatherFrame = new Stage();
+        
+
+        //Fonts
+        Font fontHeadingLabel = Font.font("arial", FontWeight.BOLD, 20);
+
+        //Weather Search Bar
+        weatherSearchField = new TextArea();
+        weatherSearchField.setTranslateX(250);
+        weatherSearchField.setTranslateY(25);
+        weatherSearchField.setMaxWidth(310);
+        weatherSearchField.setMaxHeight(0);
+
+        //Buttons:
+
+        Button searchButton = new Button("Search Weather");
+        searchButton.setTranslateX(340);
+        searchButton.setTranslateY(50);
+
+        //Labels
+
+        Label heading_Label = new Label("Search Current Weather");
+        heading_Label.setFont(fontHeadingLabel);
+        heading_Label.setTranslateX(270);
+        heading_Label.setTranslateY(10);
+
+
 
         VBox weatherBox = new VBox();
+        weatherBox.getChildren().addAll(heading_Label, weatherSearchField, searchButton);
         
-        Scene weatherScene = new Scene(weatherBox, 600, 600);
+        Scene weatherScene = new Scene(weatherBox, 800, 800);
         weatherFrame.setScene(weatherScene);
         weatherFrame.show();
+        weatherFrame.setResizable(false);
     }
 
-    // Method to connect to the API 
+    // Method to build URL with user input
 
     public void searchWeather(String weatherUserInput) {
 
@@ -113,14 +148,18 @@ public class dashboard extends Application{
             e.printStackTrace();
         }
     }
+        // Build API url with the user input
 
     private static String buildWeatherApiUrl(String weatherUserInput) {
         return String.format("%s?q=%s&appid=%s&units=metric", weatherApiUrl, weatherUserInput, weatherApiKey);
     }
 
+    //  Display Weather Data
     private void parseWeatherData(String getWeatherData){
 
         JSONObject json = new JSONObject(getWeatherData);
+        
+        // LEFT OFF HERE:
         
 
 
